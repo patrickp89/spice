eeec49b1e3a0161ddf8e4fa02fea935f540338bb
patrickp89 <patrickpreuss@netherspace.de>
2019-04-02 17:12:28 +0200

    wip: added some tree impl stub code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # spice
 A tool to find and manage duplicate files
+
+## How to build it
+Install [OPAM](https://opam.ocaml.org/doc/Install.html), on Debian run:
+```bash
+# apt-get install opam
+```
+Then install dune and Jane Street's base libraries via OPAM by running:
+```bash
+$ opam install dune base stdio
+```
+Clone or download this repository, and build by running
+```bash
+$ dune build src/spice.exe
+```
--- src/spice.ml
+++ src/spice.ml
@@ -2,33 +2,75 @@
 * Calculates MD5 hash sums for all files in a given directory.
 *)
 
+(*open Base
+open Stdio*)
+
+
+type 'a tree =
+  | Leaf
+  | Node of 'a * 'a tree * 'a tree ;;
+
+
+(* TODO: the insert function must insert the _hash_ _lexicographically_! *)
+let rec insert t x =
+  match t with
+  | Leaf -> Node(x, Leaf, Leaf)
+  | Node(y, l, r) ->
+      if x = y then t
+      else if x < y then Node(y, (insert l x), r)
+      else Node(y, l, (insert r x))
+
+
+let rec size t =
+  match t with
+  | Leaf -> 0
+  | Node (_, l, r) -> 1 + (size l) + (size r)
+
+
 (* Logs the concatenated string (u +...+ v) to stdout. *)
 let log args =
   print_endline (String.concat "" args) ;;
 
-(* Reads all files and folders in d. *)
-let files_in_dir d =
-  Sys.readdir d ;;
 
 (* Concatenates a directory name d and a filename f. *)
 let full_path d f =
   String.concat "" [d; f] ;;
 
+
 (* Calculates an MD5 hash sum for a file with the full path f. *)
 let calculate_hash_for_file f =
   Digest.to_hex(Digest.file f) ;;
 
-(* Prints the MD5 hash sum for file f in directory d. *)
-let handle_file d f =
+
+(* Calculates an MD5 hash sum for a given file f and inserts the hash into the given tree t. *)
+let calculate_and_insert_hash_into_t f t =
+  log ["md5sum("; f; ")"] ;
+  let h = calculate_hash_for_file f in
+  log [f; h] ;
+  (* TODO: insert the hash 'insert t h ;' *)
+  log ["Tree has "; string_of_int (size t); " nodes"] ;;
+
+
+(* Places the MD5 hash sum for file f in directory d into the tree t of MD5 sums. Returns the new tree. *)
+let handle_file d f t =
   let p = full_path d f in
-  log ["md5sum("; p; ")"] ;
-  let h = calculate_hash_for_file p in
-  log [f; h] ;;
+  if not (Sys.is_directory p)
+  then calculate_and_insert_hash_into_t p t
+  else log [p; " is a directory!"] ;; (* TODO; recursive descent! *)
+
+
+(* Reads all files and folders in d. *)
+let files_in_dir d =
+  Sys.readdir d ;;
+
 
 (* Prints all files and folders in d. *)
 let print_files_in_dir d =
   log [d; ":"] ;
-  Array.iter (fun f -> handle_file d f) (files_in_dir d) ;;
+  let t = Leaf in
+  (* TODO: insert a root -> 'insert t d ;' *)
+  Array.iter (fun f -> (handle_file d f t)) (files_in_dir d) ;;
+
 
 (* Main method. *)
 let () =
